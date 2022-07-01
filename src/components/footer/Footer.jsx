c5d035063476b3389b4ce7e453876b9fd02c1ba2
ricardo-sousa <rwmsousa@gmail.com>
2022-07-01 11:09:32 -0300

    fix: correct links noreferrer in the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,18 +11,22 @@ function SimpleFooter() {
         Developed by Ricardo Sousa
       </div>
       <div className="social-media">
-        <Link
-          to="https://github.com/ricardo-sousa-dev"
+        <a
+          href="https://github.com/ricardo-sousa-dev"
+          target="_blank"
+          rel="noopener noreferrer"
           className="link-github"
         >
           <FaGithub className="icon-social-media"/>
-        </Link>
-        <Link
-          to="https://www.linkedin.com/in/rwmsousa/"
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rwmsousa/"
+          target='_blank'
+          rel="noopener noreferrer"
           className="link-linkedin"
         >
           <GrLinkedin className="icon-social-media"/>
-        </Link>
+        </a>
       </div>
     </div>
   );
